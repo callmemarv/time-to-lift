a8ea9774ab8846a7712f699a2cf98fce67ed31b5
Marvin <marvin.therolf@gmail.com>
2023-12-14 12:38:32 +0100

    Use Wake Lock API if possible

--- code/src/components/utility/WakeLock.tsx
+++ code/src/components/utility/WakeLock.tsx
@@ -1,9 +1,44 @@
-import video from 'assets/black.mp4'
+import { useCallback, useEffect, useState } from 'react'
 
 export function WakeLock() {
+  const [wakeLock, setWakeLock] = useState<WakeLockSentinel>()
+
+  const requestWakeLock = useCallback(() => {
+    navigator.wakeLock.request('screen')
+      .then(setWakeLock)
+      .catch(console.error)
+  }, [])
+
+  const releaseWakeLock = useCallback(() => {
+    wakeLock?.release()
+      .then(() => {
+        setWakeLock(undefined)
+      })
+      .catch(console.error)
+  }, [])
+
+  useEffect(() => {
+    if ('wakeLock' in navigator) {
+      requestWakeLock()
+      document.addEventListener('visibilitychange', requestWakeLock)
+
+      return () => {
+        releaseWakeLock()
+        document.removeEventListener('visibilitychange', requestWakeLock)
+      }
+    }
+  }, [requestWakeLock, releaseWakeLock])
+
   return (
-    <video autoPlay loop muted className={'w-0 h-0'}>
-      <source src={video} type="video/mp4"/>
-    </video>
+    <>
+      {!wakeLock &&
+        <video autoPlay loop muted className={'w-0 h-0'}>
+          <source
+            src={'https://github.com/callmemarv/time-to-lift/raw/main/code/src/assets/black.mp4'}
+            type={'video/mp4'}
+          />
+        </video>
+      }
+    </>
   )
 }
